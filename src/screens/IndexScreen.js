1c2c6c1d562fed24cb54a392dec12486a92ac1a7
smulionOong <1224police@likelion.org>
2020-10-12 03:28:22 +0900

    Deleting Post

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,10 @@
 import React,{ useContext } from 'react';
-import {View, Text, StyleSheet, FlatList, Button} from 'react-native';
+import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from 'react-native';
 import {Context} from '../context/BlogContext';
 import {Feather} from '@expo/vector-icons';
 
 const IndexScreen = () => {
-  const {state, addBlogPost} = useContext(Context);
+  const {state, addBlogPost,deleteBlogPost} = useContext(Context);
 
   return(
     <View>
@@ -14,10 +14,16 @@ const IndexScreen = () => {
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           //item === our individual blogpost objects
-          return <View style={styles.row}>
-            <Text style={styles.title}>{item.title}</Text>
-            <Feather style={styles.icon} name="trash" />
-          </View>
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>
+                {item.title} - {item.id}
+              </Text>
+              <TouchableOpacity onPress={() => {deleteBlogPost(item.id), console.log(item.id)}}>
+                <Feather style={styles.icon} name="trash" />
+              </TouchableOpacity>
+            </View>
+          );
         }}
       />
     </View>
