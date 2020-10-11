8018e0d7148e4b4919a76642eac887043576de9f
smulionOong <1224police@likelion.org>
2020-10-12 06:06:18 +0900

    ~ Header Navigation

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,7 +3,7 @@ import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from 'react-
 import {Context} from '../context/BlogContext';
 import {Feather} from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation}) => {
   const {state, addBlogPost,deleteBlogPost} = useContext(Context);
 
   return(
@@ -15,19 +15,33 @@ const IndexScreen = () => {
         renderItem={({ item }) => {
           //item === our individual blogpost objects
           return (
-            <View style={styles.row}>
-              <Text style={styles.title}>
-                {item.title} - {item.id}
-              </Text>
-              <TouchableOpacity onPress={() => {deleteBlogPost(item.id), console.log(item.id)}}>
-                <Feather style={styles.icon} name="trash" />
-              </TouchableOpacity>
-            </View>
+            <TouchableOpacity 
+              onPress={()=>navigation.navigate('Show',{id:item.id})}
+            >
+              <View style={styles.row}>
+                <Text style={styles.title}>
+                  {item.title} - {item.id}
+                </Text>
+                <TouchableOpacity onPress={() => {deleteBlogPost(item.id), console.log(item.id)}}>
+                  <Feather style={styles.icon} name="trash" />
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
           );
         }}
       />
     </View>
   );
+};
+
+IndexScreen.navigationOptions = ({navigation}) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={()=>navigation.navigate('Create')}>
+        <Feather name="plus" size={30} />
+      </TouchableOpacity>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
